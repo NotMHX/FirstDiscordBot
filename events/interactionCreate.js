cca1d8a73df0e63edb387764f759a2dd955f11bf
BrydenIsNotSmart <brydenwcgames@gmail.com>
2022-12-31 15:04:57 -0600

    Changes

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,11 +3,14 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
-
-		if (interaction.isButton()) {
-			console.log(interaction);
+		
+        if (interaction.isButton()) {
+			if(interaction.customId === "primary") {
+				interaction.reply({ content: "Hey, you clicked me!", ephemeral: true})
+			} // This did not console log the button because you have already return if it wasn't a slash command.
 		}
+
+		if (!interaction.isChatInputCommand()) return;
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
@@ -18,10 +21,13 @@ module.exports = {
 
 		try {
 			await command.execute(interaction);
-            await console.log(`Executed the ${interaction.commandName} command.`);
+			/* Might cause console spam but use if you would like,
+            console.log(`${interaction.user.tag} has executed the ${interaction.commandName} command in ${interaction.guild.name}.`);
+			*/
 		} catch (error) {
+			await interaction.reply({ content: `There was an error while executing this command.\n\`\`\`js\n${error}\`\`\``, ephemeral: true}).catch(() => null);
+			console.error(`Error executing ${interaction.commandName}.`);
 			console.error(error);
-            console.error(`Error executing ${interaction.commandName}.`);
 		}
 	},
 };
